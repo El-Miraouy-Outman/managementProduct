0c56d19004b825c52062e10e5216a61ae621e0ac
EL-MIRAOUY <elmiraouyoutman2022@gmail.com>
2023-05-15 18:28:11 +0100

    first

---
+++ review-service/src/main/java/com/mosine/reviewservice/model/Review.java
@@ -0,0 +1,32 @@
+package com.mosine.reviewservice.model;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+
+
+@Entity
+@NoArgsConstructor
+@AllArgsConstructor
+@Getter
+@Setter
+public class Review {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long review_id;
+
+    private Long product_id;
+
+    private String author;
+
+    private String subject;
+
+    private String content;
+}
---
+++ review-service/src/main/java/com/mosine/reviewservice/service/ReviewService.java
@@ -0,0 +1,35 @@
+package com.mosine.reviewservice.service;
+
+
+import com.mosine.reviewservice.model.Review;
+import com.mosine.reviewservice.repository.ReviewRepository;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+
+@Service
+@RequiredArgsConstructor
+public class ReviewService {
+
+    private final ReviewRepository reviewRepository;
+
+
+    public Review addReview(Review review) {
+        return reviewRepository.save(review);
+    }
+
+    public Review getReview(Long id) {
+        return reviewRepository.findById(id).orElseThrow();
+    }
+
+    public void deleteReview(Long id) {
+        reviewRepository.deleteById(id);
+    }
+
+    public Review updateReview(Long id, Review review) {
+        Review reviewToUpdate = reviewRepository.findById(id).orElseThrow();
+        reviewToUpdate.setAuthor(review.getAuthor());
+        reviewToUpdate.setSubject(review.getSubject());
+        reviewToUpdate.setContent(review.getContent());
+        return reviewRepository.save(reviewToUpdate);
+    }
+}
